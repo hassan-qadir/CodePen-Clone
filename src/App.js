3f9fb4988aba2469e08a81368b5918047f3d2993
Hassan Qadir <77539962+hassan-qadir@users.noreply.github.com>
2023-04-10 23:40:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ useEffect(()=>{
 
   return (
    <>
-   <nav className="navbar navbar-light bg-light">
+   <nav className="navbar navbar-light bg-dark">
   <div className="container-fluid">
     <a className="navbar-brand p-1">
       <img src="https://www.pasha.org.pk/pashapk/2019/07/code-district.png" alt="image" width="30" height="30" className="d-inline-block align-text-top"/>
